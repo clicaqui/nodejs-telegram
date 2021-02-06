ff4de2d53198ab2be94c2d324d727bcee42e1f09
clicaqui <clicaqui@Mac-mini-de-Daniel.local>
2021-02-06 17:33:27 -0300

    Vamos la

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require("axios");
+const fetch = require("node-fetch");
 
 const app = express();
 
@@ -36,12 +37,12 @@ app.post('/start_bot', (req, res) => {
     let msg = message.text.toLowerCase().split(" ");
     const book = msg[1].charAt(0).toUpperCase() + msg[1].slice(1);
     const passage = book + msg[2] + '.' + msg[3];
-    const result = fetch(`https://api.biblia.com/v1/bible/content/LEB.html?passage=${passage}&key=${process.env.BOOK_KEY}`);
+    const result = await fetch(`https://api.biblia.com/v1/bible/content/LEB.html?passage=${passage}&key=${process.env.BOOK_KEY}`);
     console.log(result);
-    if (result){
-      reply = result + " - " + passage;  
-    } else {
+    if (!result.ok){
       reply = "Passage not found";
+    } else {
+      reply = result + " - " + passage;  
     }
   }  
       sendMessage(telegram_url, message, reply, res);
