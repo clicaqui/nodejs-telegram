1ba05e24ace10a677aefaf1aecc17b811e2264a9
clicaqui <clicaqui@Mac-mini-de-Daniel.local>
2021-02-06 13:16:53 -0300

    Vamos la

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ let userGoal = 'Learn Docker!';
 
 const API_KEY = "1638958493:AAEMBww7qvTjpo-6vXIhvS4LRIbARtNn1wU";
 
-const url = "https://api.telegram.org/bot";
+const telegram_url = `https://api.telegram.org/bot${API_KEY}/sendMessage`;
 
+app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: false,
@@ -19,17 +20,34 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
 
-  console.log(`Entrou pelo GET ${JSON.stringify(request)}`);
+  console.log(`Entrou pelo GET ${JSON.stringify(req.params)}`);
 });
 
-app.post('/', (req, res) => {
-  const enteredGoal = req.body.goal;
+app.post('/start', (req, res) => {
 
-  console.log(`Entrou pelo POST ${JSON.stringify(request)}`);
-  userGoal = enteredGoal;
-  //res.redirect('/');
-  res.redirect(url + API_KEY + `/sendMessage?chat_id=1067356804&text=${userGoal}`);
+  const { message } = req.body;
+  let reply = "Welcome to telegram weather bot";
+  let city_check = message.text.toLowerCase().indexOf('/');
+  if(message.text.toLowerCase().indexOf("hi") !== -1){
+      sendMessage(telegram_url,message,reply,res);
+  }else{
+      reply = "request not understood, please review and try again.";
+      sendMessage(telegram_url,message,reply,res);
+      return res.end();
+  }
+  
 });
+
+const sendMessage = (url, message, reply, res) => {
+    axios.post(url, { chat_id: message.chat.id,
+        text: reply
+    }).then(response => {
+        console.log("Message posted");
+        res.end("ok");
+    }).catch(error =>{
+        console.log(error);
+    });
+  }
 
 let port = process.env.PORT || 3000;
 app.listen(port);
