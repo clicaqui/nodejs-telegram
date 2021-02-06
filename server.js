3fd552942624b1154a728dda1636947c52c6b327
clicaqui <clicaqui@Mac-mini-de-Daniel.local>
2021-02-06 15:52:42 -0300

    Vamos la

--- server.js
+++ server.js
@@ -19,29 +19,33 @@ function sendMessage(url, message, reply, res) {
         text: reply
     }).then(response => {
         console.log("Message posted");
-        res.end("ok");
-    }).catch(error =>{
-        console.log(error);
-    });
-}
-app.post('/start_bot', (req, res) => {
-  const { message } = req.body;
-  const url = telegram_url;
-  console.log(message.text );
-  let reply = "Olá bem vindo ao Mensagens Biblicas";
-  if(message.text.toLowerCase().indexOf("hi") === 0){
-    reply = "Escolha um comando para ir adiante (Type '/').";
-  }
-      axios.post(url, { chat_id: message.chat.id,
-        text: reply
-    }).then(response => {
-        console.log("Message posted");
-        return res.end("ok");
+       return res.end("ok");
     }).catch(error =>{
         console.log(error);
         return;
     });
+}
+app.post('/start_bot', (req, res) => {
+  const { message } = req.body;
+  console.log(message.text );
+  
+  let reply = "Escolha um comando para ir adiante (Type: '/' ).";
+  if(message.text.toLowerCase().indexOf("hi") === 0){
+    reply = "Olá você está no Mensagens Biblicas";
+  } else if(message.text.toLowerCase().indexOf("/phrases") === 0){
+     res.redirect("/phrases");
+  }  
+      sendMessage(telegram_url, message, reply, res);
  //     return res.end();
+    
+});
+
+app.post('/start_bot', (req, res) => {
+  const { message } = req.body;
+
+  let reply = "Você está no Phrases.";
+
+  sendMessage(telegram_url, message, reply, res);
     
 });
 
