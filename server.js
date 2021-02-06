9a6bc9f925f87f69f59466cd3ede45e72f2ff857
clicaqui <clicaqui@Mac-mini-de-Daniel.local>
2021-02-06 19:11:27 -0300

    Vamos la[A

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ function sendMessage(url, message, reply, res) {
 };
 
 app.post('/start_bot', (req, res) => {
-  const { result } = req.body;
-    console.log(result.message);
+  const { message } = req.body;
+    console.log(message);
     
   /* let reply = "Hi, find your passage on the Bible...";
   if(message.text.toLowerCase().indexOf("hi") === 0){
