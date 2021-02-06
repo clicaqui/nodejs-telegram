9336b7bd0034489e5c4bc812652d3f872084d717
clicaqui <clicaqui@Mac-mini-de-Daniel.local>
2021-02-06 18:40:55 -0300

    Vamos la

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.post('/start_bot', (req, res) => {
   const { message } = req.body;
   console.log(message.text);
 
-  let reply = "Hi, find your passage on the Bible...";
+  /*let reply = "Hi, find your passage on the Bible...";
   if(message.text.toLowerCase().indexOf("hi") === 0){
     reply = "To start type: '/' )";
   } else if(message.text.toLowerCase().indexOf("/phrases") === 0){
@@ -38,13 +38,13 @@ app.post('/start_bot', (req, res) => {
     const book = msg[1].charAt(0).toUpperCase() + msg[1].slice(1);
     const passage = book + msg[2] + "." + msg[3];
 
-/*     fetch(`https://api.biblia.com/v1/bible/content/LEB.html?passage=${passage}&key=${process.env.BOOK_KEY}`).then(result => {
+    fetch(`https://api.biblia.com/v1/bible/content/LEB.html?passage=${passage}&key=${process.env.BOOK_KEY}`).then(result => {
       console.log(result);
       reply = result + " - " + passage; 
     }).catch(err => {
       reply = `Passage not found - ${err}`;
-    }); */
-  } 
+    }); 
+  } */
   sendMessage(telegram_url, message, reply, res);
  //     return res.end();
 });
