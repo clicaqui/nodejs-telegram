5b7ac00538911cdc197dd645cedb80029a58251a
clicaqui <clicaqui@Mac-mini-de-Daniel.local>
2021-02-06 19:55:06 -0300

    Vamos la[A

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ function sendMessage(url, message, reply, res) {
 };
 
 app.post('/start_bot', (req, res) => {
-  const { message } = req.body;
+  const { message } = req.body.result;
    // console.log(message.text);
     
   let reply = "Hi, find your passage on the Bible...";
