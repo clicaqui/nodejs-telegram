fb0e651046f55a44fde3d60bd9c808e411679101
clicaqui <clicaqui@Mac-mini-de-Daniel.local>
2021-02-07 00:13:19 -0300

    make it better

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-//const axios = require("axios");
+const axios = require("axios");
 
 const telegram_url = `https://api.telegram.org/bot${process.env.API_KEY}/sendMessage`;
 
