aa41e28b3bec6b01293137ab73ae02273db815f4
clicaqui <clicaqui@Mac-mini-de-Daniel.local>
2021-02-06 22:58:02 -0300

    make it better

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ function sendMessage(url, message, reply, res) {
 
 app.post('/' + process.env.API_KEY, (req, res) => {
   //console.log(req.body);
-  const { message } = req?.body;
+  const { message } = Object(req?.body);
   console.log(message);
   
     
