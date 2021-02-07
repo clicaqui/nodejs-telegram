78938eb86be1db800697190d88233210cb83ae18
clicaqui <clicaqui@Mac-mini-de-Daniel.local>
2021-02-07 11:11:02 -0300

    make it better

--- server.js
+++ server.js
@@ -45,7 +45,8 @@ app.post('/' + process.env.API_KEY, (req, res) => {
     let rnd = generateRandomPhrase(passage.length, null);
     var busca  = getHolyPassage(passage[rnd], reply);  
     busca.then(resp => {
-      reply = resp.replace('\\n\\t','');
+      console.log(resp.toString().replaceAll("\\n\\t",""));
+      reply = resp.toString().replaceAll("\\n\\t","");
        sendMessage(telegram_url, message, reply, res); 
     });
 
