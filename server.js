4d8c30d6b2b1c9fab0bd8e30fb1df38961f1fe3a
clicaqui <clicaqui@Mac-mini-de-Daniel.local>
2021-02-06 21:05:54 -0300

    make it better

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ function sendMessage(url, message, reply, res) {
     });
 };
 
-app.post('/start_bot', function(res, req) {
+app.post('/start_bot', (req, res) => {
   const { message } = req.body;
   //console.log(message);
     
