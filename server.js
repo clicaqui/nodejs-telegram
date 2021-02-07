7c3204a98748d6b9c23287259330d16e79981ef9
clicaqui <clicaqui@Mac-mini-de-Daniel.local>
2021-02-07 08:46:30 -0300

    make it better

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ function sendMessage(url, message, reply, res) {
 
 app.post('/' + process.env.API_KEY, (req, res) => {
    const { message } =  req.body;
-  console.log(message);
+  //console.log(message);
 
   let reply = "Hi, find your passage on the Bible...";
   let passage;
@@ -33,16 +33,18 @@ app.post('/' + process.env.API_KEY, (req, res) => {
   if (myEditedMessage == undefined) {
     myEditedMessage = {text: "john 1 1", chat:{id: 1067356804}};
   }
-
+ 
   if(myEditedMessage.toLowerCase().indexOf("hi") === 0){
     reply = "To start type: '/' )";
   } else if(myEditedMessage.toLowerCase().indexOf("/phrases") === 0){
-    passage = "Daniel 12 3";
-    reply = getHolyPassage(passage, reply);  
+    passage = ["Daniel12.3","John1.1"];
+    let rnd = generateRandomPhrase(passage.length, null);
+    reply = getHolyPassage(passage[rnd], reply);  
+
   } else if (myEditedMessage.toLowerCase().indexOf("") !== -1){  
     const msg = myEditedMessage.toLowerCase().split(" ");
     const book = msg[1].charAt(0).toUpperCase() + msg[1].slice(1);
-    const passage = book + msg[2] + "." + msg[3];
+    passage = book + msg[2] + "." + msg[3];
     
      reply = getHolyPassage(passage, reply);  
   } 
@@ -52,7 +54,7 @@ app.post('/' + process.env.API_KEY, (req, res) => {
 
 let port = process.env.PORT || 3000;
 app.listen(port, () => console.log("Telegram bot is listening on port 3000"));
-const generateRandomPhrase = (min, max, exclude) => {
+const generateRandomPhrase = (max, exclude) => {
   max = Math.ceil(max);
   min = Math.floor(0);
   const rand = Math.floor(Math.random() * (max - min)) + min;
