e8bfb065f8464a333c969928fc1720b25945ce8d
clicaqui <clicaqui@Mac-mini-de-Daniel.local>
2021-02-07 15:48:50 -0300

    make it better]

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ function sendMessage(url, message, reply, res) {
 };
 
 app.post('/' + process.env.API_KEY, (req, res) => {
-   const { message } =  Promisse.resolve(req.body);
+   const { message } = req.body;
    
    let reply = "Hi, find your passage on the Bible...";
    let passage;
