cb83b8195ab25b9595ea4cd23069b08120ed7591
clicaqui <clicaqui@Mac-mini-de-Daniel.local>
2021-02-07 10:54:17 -0300

    make it better

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.post('/' + process.env.API_KEY, (req, res) => {
 
   } else if (myEditedMessage.toLowerCase().indexOf("") !== -1){  
     const msg = myEditedMessage.toLowerCase().split(" ");
-    console(msg.length);
+    console.log(msg.length);
       if (msg.length > 3) {
         const book = msg[1].charAt(0).toUpperCase() + msg[1].slice(1);
         passage = book + msg[2] + "." + msg[3];
