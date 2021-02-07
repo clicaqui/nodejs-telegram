d8ae374ce1d56a635b15bff6cc1da5c1337cf371
clicaqui <clicaqui@Mac-mini-de-Daniel.local>
2021-02-07 00:02:10 -0300

    make it better

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.post('/' + process.env.API_KEY, (req, res) => {
   //console.log(message.text);
 
   let reply = "Hi, find your passage on the Bible...";
-  const myEditedMessage = Object(req.body)[1].text;
+  const myEditedMessage = Object.keys(req.body)[1].text;
   if(myEditedMessage.toLowerCase().indexOf("hi") === 0){
     reply = "To start type: '/' )";
   } else if(myEditedMessage.toLowerCase().indexOf("/phrases") === 0){
