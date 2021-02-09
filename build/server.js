9fbcd8563da1b2501a5468cd9b3b8df4aee49bab
clicaqui <clicaqui@Mac-mini-de-Daniel.local>
2021-02-09 07:03:04 -0300

    Tyscript convertion

--- build/server.js
+++ build/server.js
@@ -165,4 +165,4 @@ var getHolyPassage = function (passage, reply) { return __awaiter(void 0, void 0
 }); };
 var OLDBOOKS = ["Genesis", "Exodus", "Leviticus", "Numbers", "Deuteronomy", "Joshua", "Judges", "Samuel", "Kings", "Chronicles", "Nehemiah", "Job", "Psalm", "Proverbs", "Ecclesiastes", "Isaiah", "Jeremiah", "Ezekiel", "Daniel", "Hosea", "Joel", "Amos", "Jonah", "Naum", "Micah", "Habakkuk", "Zephaniah", "Haggai", "Malachi"];
 var NEWBOOKS = ["Matthew", "Mark", "Luke", "John", "Acts", "Romans", "Corinthians", "Galatians", "Ephesians", "Philippians", "Colossians", "Thessalonians", "Timothy", "Titus", "Hebrews", "James", "Peter", "Jude", "Revelation"];
-var RANDOM_PASSAGES = ["Daniel12.3", "John1.1", "Isaiah57.18", "Acts16.31"];
+var RANDOM_PASSAGES = ["1Chronicles29.5", "1Corinthians8.9", "John1.1", "John5.12", "John11.26", "1John3.18", "Romans5.10", "Isaiah26.4", "Isaiah57.18", "Acts16.31", "Psalm1.6", "Psalm27.14", "Psalm72.4", "Luke21.32", "Luke22.26", "Psalm6.9", "Psalm23.4", "Deuteronomy31.6", "Proverbs1.10", "Revelation3.19"];
