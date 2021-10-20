a2ae674f5d64f0c76e43192d5753f97611a15bde
clicaqui <daniel@tableless.com.br>
2021-10-20 20:26:08 -0300

    Enum books

--- build/src/util/config.js
+++ build/src/util/config.js
@@ -1,39 +1,44 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
+var __createBinding =
+  (this && this.__createBinding) ||
+  (Object.create
+    ? function (o, m, k, k2) {
+        if (k2 === undefined) k2 = k;
+        Object.defineProperty(o, k2, {
+          enumerable: true,
+          get: function () {
+            return m[k];
+          },
+        });
+      }
+    : function (o, m, k, k2) {
+        if (k2 === undefined) k2 = k;
+        o[k2] = m[k];
+      });
+var __setModuleDefault =
+  (this && this.__setModuleDefault) ||
+  (Object.create
+    ? function (o, v) {
+        Object.defineProperty(o, "default", { enumerable: true, value: v });
+      }
+    : function (o, v) {
+        o["default"] = v;
+      });
+var __importStar =
+  (this && this.__importStar) ||
+  function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    if (mod != null)
+      for (var k in mod)
+        if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k))
+          __createBinding(result, mod, k);
     __setModuleDefault(result, mod);
     return result;
-};
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.BOOK_KEY = exports.APP_KEY = void 0;
-const dotenv = __importStar(require("dotenv"));
-dotenv.config();
-let path;
-switch (process.env.NODE_ENV) {
-    case "test":
-        path = `${__dirname}/../../env/.env.test`;
-        break;
-    case "production":
-        path = `${__dirname}/../../env/.env.production`;
-        break;
-    default:
-        path = `${__dirname}/../../env/.env.development`;
-}
-dotenv.config({ path: path });
+scrollTo;
 exports.APP_KEY = process.env.APP_KEY;
 exports.BOOK_KEY = process.env.BOOK_KEY;
 //# sourceMappingURL=config.js.map
